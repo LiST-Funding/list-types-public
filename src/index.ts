a6d0d0462f321b1af0ab42d8486ad224f1d6b9c8
IzikList <izik@listsettlements.com>
2025-02-22 11:53:09 +0200

    Update pccdetails

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './patient';
 export * from './settings/facilities';
-export * from './pcc/pccDetails';
-export * from './pcc/fieldsConfiguration';
 export * from './types/le'
 export * from './utils/csv/ListCSV';
+export * from './pcc/index';
---
+++ src/pcc/index.ts
@@ -0,0 +1,3 @@
+export * from './pccDetails';
+export * from './fieldsConfiguration';
+export * from './basePccData';
