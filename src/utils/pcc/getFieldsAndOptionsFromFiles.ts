55e1a4b1e2fde568de2d6f48faf4b1aa6e77b9fe
IzikList <izik@listsettlements.com>
2025-02-22 10:06:20 +0200

    Update pccdetails

--- src/utils/pcc/getFieldsAndOptionsFromFiles.ts
+++ src/utils/pcc/getFieldsAndOptionsFromFiles.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
-import csv from 'csv-parser';
+// import csv from 'csv-parser';
 const readCSVFile = (filePath: string): Promise<any[]> => {
     return new Promise((resolve, reject) => {
         const results: any[] = [];
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', () => resolve(results))
-            .on('error', (error) => reject(error));
+        // fs.createReadStream(filePath)
+        //     .pipe(csv())
+        //     .on('data', (data) => results.push(data))
+        //     .on('end', () => resolve(results))
+        //     .on('error', (error) => reject(error));
     });
 };
 
