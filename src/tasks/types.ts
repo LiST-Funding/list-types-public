b580d58a39f789b485013aaecebadf6d4f714cb4
IzikList <42133634+IzikList@users.noreply.github.com>
2025-08-14 15:34:03 +0300

    Update types.ts

--- src/tasks/types.ts
+++ src/tasks/types.ts
@@ -56,8 +56,6 @@ export interface Task <STEP_STATUS extends string = string, STEP_TYPE extends ob
     callbackData?: any;
     duration?: number;
     messages?: TaskMessage[];
-    statusCode?:string;
-
     timeId?: string;
     subType?: SUB_TYPE; 
 }
