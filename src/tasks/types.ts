9830bb5bbf86cea7fb04586af1c586e7549dfa42
mbassan-list <mosheb@listfunding.com>
2025-08-14 14:33:53 +0300

    Add `statusCode` field on task.

--- src/tasks/types.ts
+++ src/tasks/types.ts
@@ -45,6 +45,7 @@ export interface Task <STEP_STATUS extends string = string, STEP_TYPE extends ob
     taskStartTime?: Date;
     taskEndTime?: Date | string;
     status?: string;
+    statusCode?: string;
     currentStep?: string;
     steps?: STEP_TYPE[];
     stepStatus?: STEP_STATUS;
