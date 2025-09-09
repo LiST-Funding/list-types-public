4f7d10ece80e92c3980639be96e1f036428ab972
IzikList <izik@listsettlements.com>
2025-09-09 12:31:14 +0300

    Ass regionId

--- src/tasks/types.ts
+++ src/tasks/types.ts
@@ -57,5 +57,6 @@ export interface Task <STEP_STATUS extends string = string, STEP_TYPE extends ob
     messages?: TaskMessage[];
 
     timeId?: string;
-    subType?: SUB_TYPE; 
+    subType?: SUB_TYPE;
+    regionId?: string;
 }
