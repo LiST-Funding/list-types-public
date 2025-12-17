d61acf8b4063f1bc119eda209963a46aaac4ac98
vaismav <avishaiv@listsettlements.com>
2025-12-17 14:56:28 +0200

    add AIDA status mappings and parser integration

--- src/listStatus/systemsParserFunctions.ts
+++ src/listStatus/systemsParserFunctions.ts
@@ -1,5 +1,6 @@
 import { SystemSpecificStatusParser } from "./classes/SystemSpecificStatusParser";
 import { FOUR_NEXT_EHR_STATUS, FOUR_NEXT_HOSPITAL_STATUS } from "./ehrStatuses/4NextStatuses";
+import { AIDA_EHR_STATUS, AIDA_HOSPITAL_STATUS } from "./ehrStatuses/aidaStatuses";
 import { AIDIN_EHR_STATUS, AIDIN_HOSPITAL_STATUS } from "./ehrStatuses/aidenStatuses";
 import { GENERAL_EHR_STATUS, GENERAL_HOSPITAL_STATUS } from "./ehrStatuses/generalStatuses";
 import { MEDITECH_EHR_STATUS, MEDITECH_HOSPITAL_STATUS } from "./ehrStatuses/meditechStatuses";
@@ -23,4 +24,9 @@ export const meditech = new SystemSpecificStatusParser(
   export const fourNext = new SystemSpecificStatusParser(
     FOUR_NEXT_EHR_STATUS,
     FOUR_NEXT_HOSPITAL_STATUS
+)
+
+export const aida = new SystemSpecificStatusParser(
+    AIDA_EHR_STATUS,
+    AIDA_HOSPITAL_STATUS
 )
