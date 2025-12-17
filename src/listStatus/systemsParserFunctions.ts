9aaeb6bdad8ab4a4f54545f8480d320cc07af27d
vaismav <avishaiv@listsettlements.com>
2025-12-17 11:13:14 +0200

    create status parsers per system

---
+++ src/listStatus/systemsParserFunctions.ts
@@ -0,0 +1,26 @@
+import { SystemSpecificStatusParser } from "./classes/SystemSpecificStatusParser";
+import { FOUR_NEXT_EHR_STATUS, FOUR_NEXT_HOSPITAL_STATUS } from "./ehrStatuses/4NextStatuses";
+import { AIDIN_EHR_STATUS, AIDIN_HOSPITAL_STATUS } from "./ehrStatuses/aidenStatuses";
+import { GENERAL_EHR_STATUS, GENERAL_HOSPITAL_STATUS } from "./ehrStatuses/generalStatuses";
+import { MEDITECH_EHR_STATUS, MEDITECH_HOSPITAL_STATUS } from "./ehrStatuses/meditechStatuses";
+
+export const meditech = new SystemSpecificStatusParser(
+    MEDITECH_EHR_STATUS,
+    MEDITECH_HOSPITAL_STATUS
+  )
+
+  export const aidin = new SystemSpecificStatusParser(
+    AIDIN_EHR_STATUS,
+    AIDIN_HOSPITAL_STATUS
+  )
+
+    
+  export const general = new SystemSpecificStatusParser(
+    GENERAL_EHR_STATUS,
+    GENERAL_HOSPITAL_STATUS
+  )
+
+  export const fourNext = new SystemSpecificStatusParser(
+    FOUR_NEXT_EHR_STATUS,
+    FOUR_NEXT_HOSPITAL_STATUS
+)
