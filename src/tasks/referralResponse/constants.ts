964a81a35641a481a489d0d0de624c8fcf7d9393
vaismav <avishaiv@listsettlements.com>
2026-06-03 08:58:22 +0300

    add referral response task type and sub-types constants

---
+++ src/tasks/referralResponse/constants.ts
@@ -0,0 +1,7 @@
+const TASK_TYPE = 'referralResponse';
+const TASK_SUB_TYPE = Object.freeze({
+    login: "login",
+    selectPatient: "selectPatient",
+    response: "response",
+    onIdleTask:"onIdleTask",
+});
