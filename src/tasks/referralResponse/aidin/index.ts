6cb9c2c5f7ba459b31f7e1ad334e1041c3c3e55b
vaismav <avishaiv@listsettlements.com>
2026-07-06 17:34:20 +0300

    add responseReasonGroup to FacilityResponse interface

--- src/tasks/referralResponse/aidin/index.ts
+++ src/tasks/referralResponse/aidin/index.ts
@@ -6,6 +6,7 @@ export interface FacilityResponse {
     responseStatusCode: string;
     responseReason?: string;
     responseReasonCode?: string;
+    responseReasonGroup?: string;
     comment?: string;
 }
 
