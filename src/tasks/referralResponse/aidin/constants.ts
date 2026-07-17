375b8bcb3a9d11c233ee42df088a7449db95fe46
Avishai Vaisman <39646744+vaismav@users.noreply.github.com>
2026-07-17 13:01:13 +0300

    Revert "feature/snf-449-aidin-response-refactor-to-manual-actions"

--- src/tasks/referralResponse/aidin/constants.ts
+++ src/tasks/referralResponse/aidin/constants.ts
@@ -1,7 +1,7 @@
 const AIDIN_RESPONSES = Object.freeze({
     accept: "Pre-approve",
     decline: "Decline",
-    underReview: "Under Review",
+    underReview: "Mark as Under Review",
 });
 
 type AidinResponseOption = typeof AIDIN_RESPONSES[keyof typeof AIDIN_RESPONSES];
